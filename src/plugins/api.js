167bddc162188d3e614d534620569d100bef301e
Michael Wilson <michael_wilson@sil.org>
2026-07-09 10:00:32 +0930

    resolve sonar issues in api.js

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import user from '@/plugins/user'
+import { HTTP_STATUS_FORBIDDEN, HTTP_STATUS_UNAUTHORIZED } from '../main'
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
   withCredentials: true,
@@ -8,9 +9,9 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response.data,
   (error) => {
-    const e = (error.response && error.response.data) || error
+    const e = error.response?.data || error
 
-    if ([401, 403].includes(e.status)) {
+    if ([HTTP_STATUS_UNAUTHORIZED, HTTP_STATUS_FORBIDDEN].includes(e.status)) {
       user.login(location.hash)
     }
 
